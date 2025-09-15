caf05f5ffd93fdabed2840582d3c095427867e4c
iamrazzakk <rakibt23p@gmail.com>
2025-09-15 17:23:11 +0600

    proper error handle

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,40 +1,40 @@
 import dotenv from "dotenv";
 import path from "path";
-dotenv.config({path: path.join(process.cwd(), '.env')});
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
-export default{
-    ip_address: process.env.IP,
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL,
-    node_env: process.env.NODE_ENV,
-    bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS!,
-    jwt: {
-        jwt_secret: process.env.JWT_SECRET,
-        jwt_expire_in: process.env.JWT_EXPIRE_IN,
-        jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
-        jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN
-    },
-    stripe:{
-        stripeSecretKey:process.env.STRIPE_API_SECRET,
-        webhookSecret:process.env.WEBHOOK_SECRET,
-        paymentSuccess:process.env.WEBHOOK_SECRET
-    },
-    email:{
-        from: process.env.EMAIL_FROM,
-        user: process.env.EMAIL_USER,
-        port: process.env.EMAIL_PORT,
-        host: process.env.EMAIL_HOST,
-        pass: process.env.EMAIL_PASS
-    },
-    social: {
-        google_client_id: process.env.GOOGLE_CLIENT_ID,
-        facebook_client_id: process.env.FACEBOOK_CLIENT_ID,
-        google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        facebook_client_secret: process.env.FACEBOOK_CLIENT_SECRET
-    },
-    admin: {
-        email:process.env.SUPER_ADMIN_EMAIL,
-        password: process.env.SUPER_ADMIN_PASSWORD,
-        confirmPassword: process.env.SUPER_ADMIN_PASSWORD
-    }
-}
+export default {
+  ip_address: process.env.IP,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  node_env: process.env.NODE_ENV,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS!,
+  jwt: {
+    jwt_secret: process.env.JWT_SECRET,
+    jwt_expire_in: process.env.JWT_EXPIRE_IN,
+    jwtRefreshSecret: process.env.JWT_SECRET!,
+    jwtRefreshExpiresIn: process.env.JWT_EXPIRE_IN!,
+  },
+  stripe: {
+    stripeSecretKey: process.env.STRIPE_API_SECRET,
+    webhookSecret: process.env.WEBHOOK_SECRET,
+    paymentSuccess: process.env.WEBHOOK_SECRET,
+  },
+  email: {
+    from: process.env.EMAIL_FROM,
+    user: process.env.EMAIL_USER,
+    port: process.env.EMAIL_PORT,
+    host: process.env.EMAIL_HOST,
+    pass: process.env.EMAIL_PASS,
+  },
+  social: {
+    google_client_id: process.env.GOOGLE_CLIENT_ID,
+    facebook_client_id: process.env.FACEBOOK_CLIENT_ID,
+    google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    facebook_client_secret: process.env.FACEBOOK_CLIENT_SECRET,
+  },
+  admin: {
+    email: process.env.SUPER_ADMIN_EMAIL,
+    password: process.env.SUPER_ADMIN_PASSWORD,
+    confirmPassword: process.env.SUPER_ADMIN_PASSWORD,
+  },
+};
