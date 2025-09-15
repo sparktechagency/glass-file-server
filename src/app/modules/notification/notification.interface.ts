bb72449b63a521458681a9e9d17e0b6ea7505fa9
iamrazzakk <rakibt23p@gmail.com>
2025-09-15 16:45:22 +0600

    initial commit

---
+++ src/app/modules/notification/notification.interface.ts
@@ -0,0 +1,13 @@
+import { Model, Types } from "mongoose";
+
+export type INotification = {
+  text: string;
+  receiver?: Types.ObjectId;
+  sender?: Types.ObjectId;
+  read: boolean;
+  referenceId?: string;
+  screen?: "RESERVATION" | "CHAT";
+  type?: "ADMIN";
+};
+
+export type NotificationModel = Model<INotification>;
